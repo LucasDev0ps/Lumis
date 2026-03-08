908617a8192e96916cd6104b97141d84aea482c0
Lucas Batista <95498177+LucasDev0ps@users.noreply.github.com>
2026-03-08 03:02:05 -0300

    Criar app.js

---
+++ app.js
@@ -0,0 +1,68 @@
+async function addTransaction(){
+
+let desc = document.getElementById("desc").value
+let valor = document.getElementById("valor").value
+
+if(!desc || !valor){
+alert("Preencha descrição e valor")
+return
+}
+
+await fetch(`${SUPABASE_URL}/rest/v1/transactions`,{
+
+method:"POST",
+
+headers:{
+"apikey":SUPABASE_KEY,
+"Authorization":`Bearer ${SUPABASE_KEY}`,
+"Content-Type":"application/json",
+"Prefer":"return=minimal"
+},
+
+body:JSON.stringify({
+
+description:desc,
+amount:parseFloat(valor)
+
+})
+
+})
+
+document.getElementById("desc").value=""
+document.getElementById("valor").value=""
+
+loadTransactions()
+
+}
+
+async function loadTransactions(){
+
+let res = await fetch(`${SUPABASE_URL}/rest/v1/transactions?select=*`,{
+
+headers:{
+"apikey":SUPABASE_KEY,
+"Authorization":`Bearer ${SUPABASE_KEY}`
+}
+
+})
+
+let data = await res.json()
+
+let lista = document.getElementById("lista")
+
+lista.innerHTML=""
+
+data.reverse().forEach(t=>{
+
+lista.innerHTML+=`
+<li>
+<strong>${t.description}</strong><br>
+R$ ${t.amount}
+</li>
+`
+
+})
+
+}
+
+loadTransactions()
