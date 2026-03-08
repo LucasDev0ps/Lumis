b6a07604cacff5c0a9df33c799eb58c96a9b2c2c
Lucas Batista <95498177+LucasDev0ps@users.noreply.github.com>
2026-03-08 03:34:41 -0300

    Atualizar o Dashboard.js

--- Dashboard.js
+++ Dashboard.js
@@ -1,62 +1,18 @@
-async function loadDashboard(){
-
-let res = await fetch(`${SUPABASE_URL}/rest/v1/transactions?select=*`,{
-
-headers:{
-"apikey":SUPABASE_KEY,
-"Authorization":`Bearer ${SUPABASE_KEY}`
-}
-
-})
-
-let data = await res.json()
-
-let total = 0
-
-let labels = []
-let valores = []
+let categorias = {}
 
 data.forEach(t=>{
 
-total += parseFloat(t.amount)
+let cat = t.category || "Outros"
 
-labels.push(t.description)
-valores.push(t.amount)
+if(!categorias[cat]){
+
+categorias[cat] = 0
+
+}
+
+categorias[cat] += parseFloat(t.amount)
 
 })
 
-document.getElementById("total").innerText =
-"R$ " + total.toFixed(2)
-
-document.getElementById("saldo").innerText =
-"R$ " + (6300 - total).toFixed(2)
-
-drawChart(labels,valores)
-
-}
-
-function drawChart(labels,valores){
-
-let ctx = document.getElementById("grafico")
-
-new Chart(ctx,{
-
-type:"pie",
-
-data:{
-
-labels:labels,
-
-datasets:[{
-
-data:valores
-
-}]
-
-}
-
-})
-
-}
-
-loadDashboard()
+let labels = Object.keys(categorias)
+let valores = Object.values(categorias)
