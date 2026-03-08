1ebcdceb246f09636954f5dab2c52ec5621e1161
Lucas Batista <95498177+LucasDev0ps@users.noreply.github.com>
2026-03-08 03:36:16 -0300

    Atualizar o Dashboard.js

--- Dashboard.js
+++ Dashboard.js
@@ -1,6 +1,25 @@
+async function loadDashboard(){
+
+let res = await fetch(`${SUPABASE_URL}/rest/v1/transactions?select=*`,{
+
+headers:{
+"apikey":SUPABASE_KEY,
+"Authorization":`Bearer ${SUPABASE_KEY}`
+}
+
+})
+
+let data = await res.json()
+
+let total = 0
+
 let categorias = {}
 
 data.forEach(t=>{
+
+let valor = parseFloat(t.amount)
+
+total += valor
 
 let cat = t.category || "Outros"
 
@@ -10,9 +29,51 @@ categorias[cat] = 0
 
 }
 
-categorias[cat] += parseFloat(t.amount)
+categorias[cat] += valor
 
 })
 
+document.getElementById("total").innerText =
+"R$ " + total.toFixed(2)
+
+document.getElementById("saldo").innerText =
+"R$ " + (6300 - total).toFixed(2)
+
 let labels = Object.keys(categorias)
 let valores = Object.values(categorias)
+
+drawChart(labels,valores)
+
+}
+
+function drawChart(labels,valores){
+
+let ctx = document.getElementById("grafico")
+
+if(window.chart){
+
+window.chart.destroy()
+
+}
+
+window.chart = new Chart(ctx,{
+
+type:"pie",
+
+data:{
+
+labels:labels,
+
+datasets:[{
+
+data:valores
+
+}]
+
+}
+
+})
+
+}
+
+loadDashboard()
